d060b14b588563f22ef9eb6f86b6f64f7c9c4961
zb255 <notions_flocons.6x@icloud.com>
2026-09-06 18:44:11 +0200

    Add variables and assertions for logic table 3c

--- 1E1-DM1/main.py
+++ 1E1-DM1/main.py
@@ -93,3 +93,16 @@ assertions_3a = [
 ]
 
 construct_table(Logic3, variables_3a, assertions_3a, "3a.csv")
+
+
+variables_3c = ["cal(P), cal(Q)"]
+
+assertions_3c = [
+    (lambda P, Q:~P, "not(cal(P))"),
+    (lambda P, Q:P >> Q, "cal(P) => cal(Q)"),
+    (lambda P, Q:~P >> Q, "not(cal(P)) => cal(Q)"),
+    (lambda P, Q:(P >> Q) & (~P >> Q), "(cal(P) => cal(Q)) and (not(cal(P)) => cal(Q))"),
+    (lambda P, Q:((P >> Q) & (~P >> Q)) >> Q, "((cal(P) => cal(Q)) and (not(cal(P)) => cal(Q))) => cal(Q)"),
+]
+
+construct_table(Logic3, variables_3c, assertions_3c, "3c.csv")
